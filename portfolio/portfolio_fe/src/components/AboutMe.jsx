fa87059d9f1634028600cbf92f5bf7634f1e0f32
jlputter <jputter@luc.edu>
2022-01-02 21:46:11 +0100

    final styling for header

--- portfolio/portfolio_fe/src/components/AboutMe.jsx
+++ portfolio/portfolio_fe/src/components/AboutMe.jsx
@@ -1,9 +1,13 @@
 import { Container } from "./styles/Container.styled";
+import { StyledAbout } from "./styles/AboutMe.styled";
 
-export default function Header() {
+export default function AboutMe() {
     return (
+        <StyledAbout>
         <Container>
-            
+            {/* Image and text saying who I am, what I do
+            -   text with icons for each thing I do; cs, food, pottery */}
         </Container>
+        </StyledAbout>
     )
 }
